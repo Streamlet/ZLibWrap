23a701e85751236687ff5eeb9247dcabefe21cdb
streamlet <streamlet@outlook.com>
2022-11-27 13:39:27 +0800

    migrate to gn+ninja build system

---
+++ src/ZLibWrap/ZLibWrap.cpp
@@ -0,0 +1,29 @@
+//------------------------------------------------------------------------------
+//
+//    Copyright (C) Streamlet. All rights reserved.
+//
+//    File Name:   ZLibWarp.cpp
+//    Author:      Streamlet
+//    Create Time: 2010-09-22
+//    Description: 
+//
+//    Version history:
+//
+//
+//
+//------------------------------------------------------------------------------
+
+
+#include "ZLibWrap.h"
+#include "../ZLibWrapLib/ZLibWrapLib.h"
+
+BOOL ZWZipCompress(LPCTSTR lpszSourceFiles, LPCTSTR lpszDestFile, bool bUtf8 /*= false*/)
+{
+    return ZipCompress(lpszSourceFiles, lpszDestFile, bUtf8);
+}
+
+BOOL ZWZipExtract(LPCTSTR lpszSourceFile, LPCTSTR lpszDestFolder)
+{
+    return ZipExtract(lpszSourceFile, lpszDestFolder);
+}
+
